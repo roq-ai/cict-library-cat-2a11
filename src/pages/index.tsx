62ab851220a0f2a4a3a959bad906602692c80947
roq-ai <>
2023-07-03 08:57:45 +0000

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,102 @@
+import { Button, Flex, Heading, Image, Text, Stack, useBreakpointValue, Box, Link } from '@chakra-ui/react';
+
+import { signIn, signUp, requireNextAuth } from '@roq/nextjs';
+
+import Head from 'next/head';
+import { HelpBox } from 'components/help-box';
+
+function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>{`CICT Library Catalog`}</title>
+
+        <meta
+          name="description"
+          content="is your gateway to a world of knowledge. Explore, discover, and expand your horizons with CICT Library Catalog."
+        />
+      </Head>
+
+      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+          <Stack position="relative" spacing={6} w={'full'} maxW={'lg'}>
+            <HelpBox />
+            <Image src="/roq.svg" alt="Logo" w="150px" mb="8" />
+            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+              <Text as={'span'}>Explore</Text>{' '}
+              <Text
+                as={'span'}
+                position={'relative'}
+                _after={{
+                  content: "''",
+                  width: 'full',
+                  height: useBreakpointValue({ base: '20%', md: '30%' }),
+                  position: 'absolute',
+                  bottom: 1,
+                  left: 0,
+                  bg: 'cyan.400',
+                  zIndex: -1,
+                }}
+              >
+                {`CICT Library Catalog`}
+              </Text>
+            </Heading>
+            <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+              {`is your gateway to a world of knowledge. Explore, discover, and expand your horizons with CICT Library Catalog.`}
+            </Text>
+            <Stack direction="column" spacing={4} className="roles-container">
+              <Stack className="owner-roles-container" direction={{ base: 'column', md: 'row' }} spacing={4}>
+                <Button
+                  rounded={'full'}
+                  bg={'cyan.500'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'cyan.700',
+                  }}
+                  onClick={() => signUp('catalog-manager')}
+                >
+                  Create Account
+                </Button>
+                <Button rounded={'full'} onClick={() => signIn('catalog-manager')}>
+                  Login
+                </Button>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Flex>
+        <Flex position="relative" flex={1}>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1481627834876-b7833e8f5570?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NjA3NjB8MHwxfHNlYXJjaHwxfHxsaWJyYXJ5JTJDY2F0YWxvZ3xlbnwwfHx8fDE2ODgzNzQ2MTZ8MA&ixlib=rb-4.0.3&q=80&w=1080'
+            }
+            alt={'Login Image'}
+            objectFit={'cover'}
+          />
+          <Box position="absolute" top="0" backgroundColor="rgba(0,0,0,0.6)" width="100%" py="2">
+            <Text align="center" fontSize="sm" color="white">
+              Photo by{' '}
+              <Link
+                href="https://unsplash.com/@itfeelslikefilm?utm_source=roq-generator&utm_medium=referral"
+                isExternal
+                color="teal.200"
+              >{`🇸🇮 Janko Ferlič`}</Link>{' '}
+              on{' '}
+              <Link
+                href="https://unsplash.com/?utm_source=roq-generator&utm_medium=referral"
+                isExternal
+                color="teal.200"
+              >
+                Unsplash
+              </Link>
+            </Text>
+          </Box>
+        </Flex>
+      </Stack>
+    </>
+  );
+}
+
+export default requireNextAuth({
+  redirectIfAuthenticated: true,
+  redirectTo: '/users',
+})(HomePage);
